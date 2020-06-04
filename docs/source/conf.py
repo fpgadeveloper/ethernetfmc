f0d627c253bb8484e1ad3c7a94d908662e41d258
Jeff Johnson <jeff@fpgadeveloper.com>
2020-06-04 15:14:15 -0400

    * added logo * added net lengths

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,3 +52,21 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_logo = 'images\ethernet-fmc-logo.png'
+
+html_theme_options = {
+    'canonical_url': '',
+    'logo_only': True,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
