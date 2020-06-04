23e57f2ab5469e49ab8fae110470bd84dee16910
Jeff Johnson <jeff@fpgadeveloper.com>
2020-06-04 15:27:21 -0400

    * changed logo to svg format

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,7 +53,7 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_logo = 'images\ethernet-fmc-logo.png'
+html_logo = 'images\ethernet-fmc-logo.svg'
 
 html_theme_options = {
     'canonical_url': '',
